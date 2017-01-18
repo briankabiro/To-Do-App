e6490e08ccd360f7986e5fc60bb59402b2d1c2ac
BrianLee <briankigondu@gmail.com>
2017-01-18 22:28:35 +0300

    Added some logic
    Added functions and Vue js code

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 /*Build HTML input box, bind to variable, build todo component
-
+when button clicked, add words in input box to todos
  */
 
+Vue.component('todo-item',{
+	template:'<li>{{ todo.text }}</li>',
+	props:['todo']	
+})
+
+
  var vm = new Vue({
- 	
+	el:"#app",
+	data:{
+		todos:[
+			{text:"John Mbad"},
+			{text:"Perter Hosi"},
+			{text:"Salazar Snape"}
+		],
+		anotherTodo:""
+	},
+	methods:{
+		addTodo(){
+			this.todos.push({text:this.todoText});
+			this.todoText=""
+		}
+	}
  })
